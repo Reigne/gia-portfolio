6e751d941d4d90c96e7ec927cff5c5af1a7f4363
Reigne <monteronaelija@yahoo.com>
2025-01-13 17:29:17 +0800

    Enhance Navbar with clickable logo and add Testimonials link; implement animations for Projects section using Framer Motion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,10 +23,13 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center py-6 px-[180px] text-white font-poppins">
-      <div className="flex items-center gap-2 cursor-pointer">
+      <a
+        className="flex items-center gap-2 cursor-pointer"
+        onClick={() => handleNavigation("home")}
+      >
         <IoHeart size={24} />
         <p className="text-4xl font-bold">Gia</p>
-      </div>
+      </a>
 
       <div>
         <ul className="flex items-center gap-14">
@@ -49,8 +52,9 @@ const Navbar = () => {
           >
             Projects
           </li>
+          <li className="cursor-pointer hover:text-purple-400">Testimonials</li>
           <Link to="/contact">
-            <li className="cursor-pointer border border-transparent bg-gradient-to-r from-purple-400 to-violet-500 bg-clip-border py-2 px-6 rounded-full shadow-lg shadow-violet-500/50">
+            <li className="cursor-pointer border border-transparent bg-gradient-to-r from-purple-400 to-violet-500 bg-clip-border py-2 px-6 rounded-full shadow-lg shadow-violet-500/50 transform transition-transform duration-400 hover:scale-110">
               Contact Me
             </li>
           </Link>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 const Projects = () => {
+  const refShortForm = useRef(null);
+  const refLongForm = useRef(null);
+  const refAds = useRef(null);
+
+  const isInViewShortForm = useInView(refShortForm, { once: true });
+  const isInViewLongForm = useInView(refLongForm, { once: true });
+  const isInViewAds = useInView(refAds, { once: true });
+
   return (
     <div id="projects" className="min-h-screen bg-gray-50 font-poppins px-[180px] py-8">
       <div className="flex flex-col justify-center items-center">
@@ -8,47 +17,155 @@ const Projects = () => {
         <p className="text-lg text-zinc-600">Here are some of my projects</p>
       </div>
 
-      {/* 3 reels size video placeholder container */}
-      <div className="mt-12 flex flex-col gap-2">
-        <p className="text-lg text-purple-500 border border-purple-400 px-6  rounded-full w-fit">SHORT-FORM</p>
+      <motion.div
+        ref={refShortForm}
+        initial={{ y: 150, opacity: 0 }}
+        animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 1 }}
+        className="mt-12 flex flex-col gap-2"
+      >
+        <p className="text-lg text-purple-500 border border-purple-400 px-6 rounded-full w-fit">
+          SHORT-FORM
+        </p>
         <div className="flex justify-between items-center gap-2">
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
+          <motion.div
+            initial={{ y: 150, opacity: 0 }}
+            animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 200, opacity: 0 }}
+            animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.2 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 250, opacity: 0 }}
+            animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.4 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
         </div>
         <div className="flex justify-between items-center gap-2">
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
+          <motion.div
+            initial={{ y: 150, opacity: 0 }}
+            animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 200, opacity: 0 }}
+            animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.2 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 250, opacity: 0 }}
+            animate={isInViewShortForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.4 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="mt-12 flex flex-col gap-2">
-        <p className="text-lg text-purple-500 border border-purple-400 px-6  rounded-full w-fit">LONG-FORM</p>
+      <motion.div
+        ref={refLongForm}
+        initial={{ y: 200, opacity: 0 }}
+        animate={isInViewLongForm ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 1.2 }}
+        className="mt-12 flex flex-col gap-2"
+      >
+        <p className="text-lg text-purple-500 border border-purple-400 px-6 rounded-full w-fit">
+          LONG-FORM
+        </p>
         <div className="flex justify-between items-center gap-2">
-          <div className="w-1/2 h-[470px] bg-zinc-200 rounded"></div>
-          <div className="w-1/2 h-[470px] bg-zinc-200 rounded"></div>
+          <motion.div
+            initial={{ y: 200, opacity: 0 }}
+            animate={isInViewLongForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.2 }}
+            className="w-1/2 h-[470px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 250, opacity: 0 }}
+            animate={isInViewLongForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.4 }}
+            className="w-1/2 h-[470px] bg-zinc-200 rounded"
+          ></motion.div>
         </div>
         <div className="flex justify-between items-center gap-2">
-          <div className="w-1/2 h-[470px] bg-zinc-200 rounded"></div>
-          <div className="w-1/2 h-[470px] bg-zinc-200 rounded"></div>
+          <motion.div
+            initial={{ y: 200, opacity: 0 }}
+            animate={isInViewLongForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.2 }}
+            className="w-1/2 h-[470px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 250, opacity: 0 }}
+            animate={isInViewLongForm ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.4 }}
+            className="w-1/2 h-[470px] bg-zinc-200 rounded"
+          ></motion.div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="mt-12 flex flex-col gap-2">
-        <p className="text-lg text-purple-500 border border-purple-400 px-6  rounded-full w-fit">ADS</p>
+      <motion.div
+        ref={refAds}
+        initial={{ y: 250, opacity: 0 }}
+        animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 1.4 }}
+        className="mt-12 flex flex-col gap-2"
+      >
+        <p className="text-lg text-purple-500 border border-purple-400 px-6 rounded-full w-fit">
+          ADS
+        </p>
         <div className="flex justify-between items-center gap-2">
-          <div className="w-[380px] h-[380px] bg-zinc-200 rounded"></div>
-          <div className="w-[380px] h-[380px] bg-zinc-200 rounded"></div>
-          <div className="w-[380px] h-[380px] bg-zinc-200 rounded"></div>
-          <div className="w-[380px] h-[380px] bg-zinc-200 rounded"></div>
+          <motion.div
+            initial={{ y: 250, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.4 }}
+            className="w-[380px] h-[380px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 300, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.6 }}
+            className="w-[380px] h-[380px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 350, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.8 }}
+            className="w-[380px] h-[380px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 400, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 2 }}
+            className="w-[380px] h-[380px] bg-zinc-200 rounded"
+          ></motion.div>
         </div>
         <div className="flex justify-between items-center gap-2">
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
-          <div className="w-[1136px] h-[640px] bg-zinc-200 rounded"></div>
+          <motion.div
+            initial={{ y: 250, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.4 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 300, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.6 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ y: 350, opacity: 0 }}
+            animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
+            transition={{ duration: 1.8 }}
+            className="w-[1136px] h-[640px] bg-zinc-200 rounded"
+          ></motion.div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
