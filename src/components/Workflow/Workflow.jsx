959b0277368270c2b439440ac383ba96fdf95d99
Reigne <monteronaelija@yahoo.com>
2025-01-23 15:47:14 +0800

    Enhance Workflow component for better responsiveness and layout adjustments

--- src/components/Workflow/Workflow.jsx
+++ src/components/Workflow/Workflow.jsx
@@ -10,14 +10,14 @@ const Workflow = () => {
     <motion.div
       ref={ref}
       id="workflow"
-      className="min-h-screen bg-white font-poppins"
+      className="min-h-screen bg-white font-poppins overflow-hidden"
       initial={{ y: 100, opacity: 0 }}
       animate={isInView ? { y: 0, opacity: 1 } : {}}
       transition={{ duration: 1 }}
     >
       <Showcase />
 
-      <div className="flex flex-col justify-center items-center px-[180px] py-8 mt-4">
+      <div className="flex flex-col justify-center items-center px-4 md:px-[180px] py-8 mt-4">
         <div className="flex flex-col justify-center items-center">
           <p className="text-purple-500 text-4xl font-bold">Workflow</p>
           <span className="text-zinc-600">
@@ -27,7 +27,7 @@ const Workflow = () => {
 
         {/* video templace container */}
         <div className="flex justify-center items-center mt-8">
-          <div className="w-[1280px] h-[650px] bg-zinc-200 rounded"></div>
+          <div className="w-full md:w-[1280px] h-[200px] md:h-[650px] bg-zinc-200 rounded"></div>
         </div>
       </div>
     </motion.div>
