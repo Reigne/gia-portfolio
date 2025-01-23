b26245672509b682b9576786632bc4306db6730f
Reigne <monteronaelija@yahoo.com>
2025-01-23 16:30:59 +0800

    Refactor Projects component for improved layout and video handling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,7 +11,10 @@ const Projects = () => {
   const isInViewAds = useInView(refAds, { once: true });
 
   return (
-    <div id="projects" className="min-h-screen bg-gray-50 font-poppins px-[180px] py-8 overflow-hidden">
+    <div
+      id="projects"
+      className="min-h-screen bg-gray-50 font-poppins px-[180px] py-8 overflow-hidden"
+    >
       <div className="flex flex-col justify-center items-center">
         <p className="text-4xl font-bold text-purple-500">Projects</p>
         <p className="text-lg text-zinc-600">Here are some of my projects</p>
@@ -24,7 +27,7 @@ const Projects = () => {
         transition={{ duration: 1 }}
         className="mt-12 flex flex-col gap-2"
       >
-        <p className="text-lg text-purple-500 border border-purple-400 px-6 rounded-full w-fit">
+        <p className="text- bg-purple-400 text-white px-6 rounded-full w-fit">
           SHORT-FORM
         </p>
         <div className="flex justify-between items-center gap-2">
@@ -124,26 +127,65 @@ const Projects = () => {
             initial={{ y: 250, opacity: 0 }}
             animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
             transition={{ duration: 1.4 }}
-            className="w-[380px] h-[380px] bg-zinc-200 rounded"
-          ></motion.div>
+            className="relative w-[380px] h-[380px] bg-zinc-200 rounded"
+          >
+            <div className="absolute top-0 left-0 p-2 w-full backdrop-filter backdrop-blur-sm">
+              <p className="text-white text-sm font-bold">
+                Option One Solar Ads
+              </p>
+            </div>
+            <video
+              src="/videos/ads/Option One Solar Ads.mp4"
+              controls
+              // poster="/images/Gia.png"
+              onMouseOver={(e) => e.target.play()}
+              onMouseOut={(e) => e.target.pause()}
+              className="w-full h-full object-cover"
+            ></video>
+          </motion.div>
           <motion.div
-            initial={{ y: 300, opacity: 0 }}
+            initial={{ y: 250, opacity: 0 }}
             animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
-            transition={{ duration: 1.6 }}
-            className="w-[380px] h-[380px] bg-zinc-200 rounded"
-          ></motion.div>
+            transition={{ duration: 1.4 }}
+            className="relative w-[380px] h-[380px] bg-zinc-200 rounded"
+          >
+            <div className="absolute top-0 left-0 p-2 w-full backdrop-filter backdrop-blur-sm">
+              <p className="text-white text-sm font-bold">
+                Option One Solar Ads
+              </p>
+            </div>
+            <video
+              src="/videos/ads/Option One Solar Ads.mp4"
+              controls
+              onMouseOver={(e) => e.target.play()}
+              onMouseOut={(e) => e.target.pause()}
+              className="w-full h-full object-cover"
+            ></video>
+          </motion.div>
           <motion.div
-            initial={{ y: 350, opacity: 0 }}
+            initial={{ y: 250, opacity: 0 }}
             animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
-            transition={{ duration: 1.8 }}
-            className="w-[380px] h-[380px] bg-zinc-200 rounded"
-          ></motion.div>
+            transition={{ duration: 1.4 }}
+            className="relative w-[380px] h-[380px] bg-zinc-200 rounded"
+          >
+            <video
+              src="/videos/ads/Option One Solar Ads.mp4"
+              controls
+              className="w-full h-full object-cover"
+            ></video>
+          </motion.div>
           <motion.div
-            initial={{ y: 400, opacity: 0 }}
+            initial={{ y: 250, opacity: 0 }}
             animate={isInViewAds ? { y: 0, opacity: 1 } : {}}
-            transition={{ duration: 2 }}
-            className="w-[380px] h-[380px] bg-zinc-200 rounded"
-          ></motion.div>
+            transition={{ duration: 1.4 }}
+            className="relative w-[380px] h-[380px] bg-zinc-200 rounded"
+          >
+            <video
+              src="/videos/ads/Option One Solar Ads.mp4"
+              controls
+              className="w-full h-full object-cover"
+            ></video>
+          </motion.div>
         </div>
         <div className="flex justify-between items-center gap-2">
           <motion.div
