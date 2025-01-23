1bf786c9ff40e5bac4eea0abd6bdf36de3baa521
Reigne <monteronaelija@yahoo.com>
2025-01-23 15:47:08 +0800

    Add Showcase component with responsive design and marquee effect

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -4,16 +4,16 @@ import { PiVideoFill } from "react-icons/pi";
 
 const Showcase = () => {
   return (
-    <div className="flex justify-between items-center gap-4 px-[180px] py-8 font-poppins  border-b border-zinc-200">
-      <p className="text-xl font-bold">MONTAGE</p>
-      <IoHeart size={24} />
-      <p className="text-xl font-bold">SHORT-FORM</p>
-      <IoHeart size={24} />
-      <p className="text-xl font-bold">LONG-FORM</p>
-      <IoHeart size={24} />
-      <p className="text-xl font-bold">TESTIMONIALS</p>
-      <IoHeart size={24} />
-      <p className="text-xl font-bold">ADS</p>
+    <div className="flex justify-between items-center gap-4 px-[10px] md:px-[20px] lg:px-[40px] xl:px-[180px] py-8 font-poppins  border-b border-zinc-200">
+      <p className="text-xs md:text-lg font-bold">MONTAGE</p>
+      <IoHeart className="md:text-[24px] text-[16px]" />
+      <p className="text-xs md:text-lg font-bold">SHORT-FORM</p>
+      <IoHeart className="md:text-[24px] text-[16px]" />
+      <p className="text-xs md:text-lg font-bold">LONG-FORM</p>
+      <IoHeart className="md:text-[24px] text-[16px]" />
+      <p className="text-xs md:text-lg font-bold">TESTIMONIALS</p>
+      <IoHeart className="md:text-[24px] text-[16px]" />
+      <p className="text-xs md:text-lg font-bold">ADS</p>
     </div>
   );
 };
