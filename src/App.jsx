93ec318a0f47ee8342449b535f60454ec2894108
Reigne <monteronaelija@yahoo.com>
2025-01-25 21:42:47 +0800

    Add Navbar and Footer components to App for improved layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </>
   );
 }
