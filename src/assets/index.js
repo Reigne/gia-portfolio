82c9b64834bc696d8aa331708ec0fc7e6e0a9a56
Reigne <monteronaelija@yahoo.com>
2025-01-27 20:04:23 +0800

    Add client logos and import them into constants for easier access

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+// import all the svg files from clients 1-8 folder
+import logoipsum1 from "./clients/logoipsum-1.svg";
+import logoipsum2 from "./clients/logoipsum-2.svg";
+import logoipsum3 from "./clients/logoipsum-3.svg";
+import logoipsum4 from "./clients/logoipsum-4.svg";
+import logoipsum5 from "./clients/logoipsum-5.svg";
+import logoipsum6 from "./clients/logoipsum-6.svg";
+import logoipsum7 from "./clients/logoipsum-7.svg";
+import logoipsum8 from "./clients/logoipsum-8.svg";
+
+export {
+  logoipsum1,
+  logoipsum2,
+  logoipsum3,
+  logoipsum4,
+  logoipsum5,
+  logoipsum6,
+  logoipsum7,
+  logoipsum8,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+// import all logo from assets clients
+import {
+  logoipsum1,
+  logoipsum2,
+  logoipsum3,
+  logoipsum4,
+  logoipsum5,
+  logoipsum6,
+  logoipsum7,
+  logoipsum8,
+} from "../assets";
+
+// cleints logo import all 1-8
+
+export const CLIENTS_LOGO = [
+  {
+    id: 1,
+    name: "Logo Ipsum 1",
+    imageUrl: logoipsum1,
+  },
+  {
+    id: 2,
+    name: "Logo Ipsum 2",
+    imageUrl: logoipsum2,
+  },
+  {
+    id: 3,
+    name: "Logo Ipsum 3",
+    imageUrl: logoipsum3,
+  },
+  {
+    id: 4,
+    name: "Logo Ipsum 4",
+    imageUrl: logoipsum4,
+  },
+  {
+    id: 5,
+    name: "Logo Ipsum 5",
+    imageUrl: logoipsum5,
+  },
+  {
+    id: 6,
+    name: "Logo Ipsum 6",
+    imageUrl: logoipsum6,
+  },
+  {
+    id: 7,
+    name: "Logo Ipsum 7",
+    imageUrl: logoipsum7,
+  },
+  {
+    id: 8,
+    name: "Logo Ipsum 8",
+    imageUrl: logoipsum8,
+  },
+];
