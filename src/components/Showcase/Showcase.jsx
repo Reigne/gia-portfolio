e7c6408b1c77752c15fd76ab354ca489438033cc
Reigne <monteronaelija@yahoo.com>
2025-04-26 02:50:11 +0800

    Add max-width and margin auto to Showcase component for improved layout

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -4,7 +4,7 @@ import { PiVideoFill } from "react-icons/pi";
 
 const Showcase = () => {
   return (
-    <div className="flex justify-between items-center gap-4 px-[10px] md:px-[20px] lg:px-[40px] xl:px-[180px] py-8 font-poppins  border-b border-zinc-200">
+    <div className="text-zinc-900 flex justify-between items-center gap-4 px-[10px] md:px-[20px] lg:px-[40px] xl:px-[180px] py-8 font-poppins  border-b border-zinc-200 max-w-[1920px] mx-auto">
       <p className="text-xs md:text-lg font-bold">MONTAGE</p>
       <IoHeart className="md:text-[24px] text-[16px]" />
       <p className="text-xs md:text-lg font-bold">SHORT-FORM</p>
